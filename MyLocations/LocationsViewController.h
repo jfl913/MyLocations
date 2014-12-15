54d24a89f8d293c8d336fb400c6b0efe8a74a89f
jfl913 <jfl913@163.com>
2014-12-15 23:36:08 +0800

    no message

---
+++ MyLocations/LocationsViewController.h
@@ -0,0 +1,15 @@
+//
+//  LocationsViewController.h
+//  MyLocations
+//
+//  Created by jfl913 on 14/12/10.
+//  Copyright (c) 2014年 jfl913. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationsViewController : UITableViewController
+
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+
+@end
